469a7940a0172b83ca002651ccf5cedda8937f1c
tafra <tafra@169.254.123.236>
2018-02-04 02:15:11 +0200

    Έναρξη Εργασίας

---
+++ src/openweather_based_weather_client/Openweather_Based_Weather_Client.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package openweather_based_weather_client;
+
+/**
+ *
+ * @author tafra
+ */
+public class Openweather_Based_Weather_Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
